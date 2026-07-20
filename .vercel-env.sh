ed9d396d1ee95bfd08e165b86a746cc152492b73
Arpit Jadav <arpit@zertech.app>
2026-07-20 12:40:03 +0530

    remove .vercel-env.sh from repo (contains secrets)

--- .vercel-env.sh
+++
@@ -1,8 +0,0 @@
-﻿#!/bin/bash
-TOKEN="vcp_3yv8p72Rk5grlbU7iN1LHV5cmIOWV8WUT8I2HOcJmypv843Jz71ecUOQ"
-cd /c/Users/jadav/Zertech
-while IFS='=' read -r key value || [ -n "$key" ]; do
-  [[ -z "$key" || "$key" =~ ^# ]] && continue
-  echo "$value" | vercel env add "$key" production --token="$TOKEN" --yes 2>&1 | tail -1
-done < .env
-
